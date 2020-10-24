985bb73fae58dd4020c7e5aaf4e44155bb074f8d
Hyun Lim <hylim1988@gmail.com>
2020-10-23 18:46:23 -0600

    add more comments

--- epidemic/epidemic_modeling.py
+++ epidemic/epidemic_modeling.py
@@ -32,28 +32,35 @@ s0 = 1 - i0
 # Solve SIR differential equations using scipy
 num_times = 180 # simulate for half a year
 times = np.arange(num_times)
-Us = np.empty((num_times,3))
-Us[0] = np.array([s0,i0,r0])
+# Define vector that conatains s,i,r
+u_vec = np.empty((num_times,3))
+# Initial data for variables
+u_vec[0] = np.array([s0,i0,r0])
 
-def rhs(t, U, beta, gamma):
-    s,i,r = U[0],U[1],U[2]
+# Define RHS function for set of ODE
+def rhs_f(t, u, beta, gamma):
+    s,i,r = u[0],u[1],u[2]
     return np.array([-beta*s*i,beta*s*i-gamma*i,gamma*i])
 
+# Call ODE integrator from scipy
 from scipy.integrate  import ode
-integrator = ode(rhs)
+integrator = ode(rhs_f)
 
 # set initial conditions and parameters
 beta,gamma = define_beta_gamma(R=5,recovery_time=14)
-integrator.set_initial_value(Us[0],0)
+integrator.set_initial_value(u_vec[0],0)
 integrator.set_f_params(beta,gamma)
 
 # Integrate ODE
 for i in range(len(times)-1):
     t = times[i+1]
     integrator.integrate(t)
-    Us[i+1] = integrator.y
+    u_vec[i+1] = integrator.y
 
-s,i,r = Us[:,0],Us[:,1],Us[:,2]
+# Save s,i,r values from results
+s,i,r = u_vec[:,0],u_vec[:,1],u_vec[:,2]
+
+# Plot the results
 plt.plot(times,s,label='susceptible')
 plt.plot(times,i,label='infected')
 plt.plot(times,r,label='recovered')
@@ -83,32 +90,39 @@ d0 = 0
 # Solve SIDR differential equation
 num_times = 180 # simulate for half a year
 times = np.arange(num_times)
-Us = np.empty((num_times,4))
-Us[0] = np.array([s0,i0,r0,d0])
+# Define a vector that contains for s,i,r,d
+u_vec = np.empty((num_times,4))
+# Initial data for variables
+u_vec[0] = np.array([s0,i0,r0,d0])
 
-def rhs(t, U, beta, gamma, sigma):
-    s,i,r,d = U[0],U[1],U[2],U[3]
+# Define RHS function for set of ODE
+def rhs(t, u, beta, gamma, sigma):
+    s,i,r,d = u[0],u[1],u[2],u[3]
     return np.array([-beta*s*i,
                      beta*s*i-(gamma+sigma)*i,
                      gamma*i,
                      sigma*i
                     ])
+
+# Call ODE integrator
 integrator = ode(rhs)
 
 # set initial conditions and parameters
 beta,gamma = define_beta_gamma(R=3.4,recovery_time=14)
 sigma = R1*gamma
-integrator.set_initial_value(Us[0],0)
+integrator.set_initial_value(u_vec[0],0)
 integrator.set_f_params(beta,gamma,sigma)
 
 # Integrate ODE
 for i in range(len(times)-1):
     t = times[i+1]
     integrator.integrate(t)
-    Us[i+1] = integrator.y
+    u_vec[i+1] = integrator.y
 
-s,i,r,d = Us[:,0],Us[:,1],Us[:,2],Us[:,3]
+# Save s,i,r,d values from results
+s,i,r,d = u_vec[:,0],u_vec[:,1],u_vec[:,2],u_vec[:,3]
 
+# Plot the results
 plt.plot(times,s,label='susceptible')
 plt.plot(times,i,label='infected')
 plt.plot(times,r,label='recovered')
